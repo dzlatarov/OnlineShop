00b43ac811dd6029943b782fd08a5e59fd8d337a
dzlatarov <danzlatarov1@gmail.com>
2020-12-04 10:33:16 +0200

    Initial commit create web application with identity user

---
+++ MasterShop/MasterShop.Data/MasterShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterShop.Data
+{
+    public class MasterShopDbContext : DbContext
+    {
+        public MasterShopDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
