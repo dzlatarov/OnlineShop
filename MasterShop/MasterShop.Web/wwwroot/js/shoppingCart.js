400605bf7c05f1f9367ff8ad6c5bc6a4f92c04fa
dzlatarov <danzlatarov1@gmail.com>
2020-12-23 12:55:07 +0200

    add logic to save data into the local storage with javascript

--- MasterShop/MasterShop.Web/wwwroot/js/shoppingCart.js
+++ MasterShop/MasterShop.Web/wwwroot/js/shoppingCart.js
@@ -1,15 +1,33 @@
-﻿var productsTargets = document.getElementsByClassName("shoppingCart");
-for (var i = 0; i < productsTargets.length; i++) {
-    productsTargets[i].addEventListener("click", function () {
-        save(event)
-    })
-}
-
-function save(event) {
+﻿function save(event) {
     var productTarget = event.target;
     var tdElements = productTarget.parentElement.parentElement.querySelectorAll('td');
-    var productNmae = tdElements[0].innerText;
+    var productName = tdElements[0].innerText;
     var description = tdElements[1].innerText;
     var SKU = tdElements[2].innerText;
-    var price = tdElements[3].innerText;    
+    var price = tdElements[3].innerText;
+    var product = {};
+    product.name = productName;
+    product.description = description;
+    product.SKU = SKU;
+    product.price = price;
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', '[]');
+    }
+    var oldData = JSON.parse(localStorage.getItem('data'));
+    if (oldData.length == 0) {
+        oldData.push(product);
+    } else {
+        var isMatch = false;
+        for (var i = 0; i < oldData.length; i++) {
+            if (oldData[i].name === productName) {
+                isMatch = true;
+                break;
+            }
+        }
+        if (!isMatch) {
+            oldData.push(product);
+        }
+    }
+
+    localStorage.setItem('data', JSON.stringify(oldData));
 }
