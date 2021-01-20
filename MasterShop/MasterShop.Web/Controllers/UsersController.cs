c6115c5d6bdd9c61040acd82890788d7992922eb
dzlatarov <danzlatarov1@gmail.com>
2021-01-20 11:52:46 +0200

    Create user controller and add users service through DI

---
+++ MasterShop/MasterShop.Web/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using MasterShop.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterShop.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+    }
+}
